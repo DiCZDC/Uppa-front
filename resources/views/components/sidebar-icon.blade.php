e80e68c0a81aeb54f40a18aa55ea1761f27c6115
Danielinh0 <danyeduardo0911@gmail.com>
2026-05-07 21:13:44 -0600

    cambios a la posicion

--- resources/views/components/sidebar-icon.blade.php
+++ resources/views/components/sidebar-icon.blade.php
@@ -6,10 +6,10 @@
 <span class="relative inline-flex size-4 items-center justify-center">
     <flux:icon
         :icon="$icon"
-        class="absolute left-1/2 top-1/2 !size-4 -translate-x-1/2 -translate-y-1/2 opacity-100 transition-opacity [[data-flux-sidebar-item]:hover_&]:opacity-0 [[data-flux-sidebar-item][data-current]_&]:opacity-0"
+        class="absolute left-1/8 top-1/7 !size-4 opacity-100 transition-opacity [[data-flux-sidebar-item]:hover_&]:opacity-0 [[data-flux-sidebar-item][data-current]_&]:opacity-0"
     />
     <flux:icon
         :icon="$iconHover"
-        class="absolute left-1/2 top-1/2 !size-4 -translate-x-1/2 -translate-y-1/2 opacity-0 transition-opacity [[data-flux-sidebar-item]:hover_&]:opacity-100 [[data-flux-sidebar-item][data-current]_&]:opacity-100"
+        class="absolute left-1/12 top-1/7 !size-4 opacity-0 transition-opacity [[data-flux-sidebar-item]:hover_&]:opacity-100 [[data-flux-sidebar-item][data-current]_&]:opacity-100"
     />
 </span>
