39dfd0f729cd2c76386d11539e6c17a4502f3d47
Danielinh0 <168349271+Danielinh0@users.noreply.github.com>
2026-05-09 20:26:21 -0600

    cambios al puerto

--- vite.config.js
+++ vite.config.js
@@ -16,7 +16,7 @@ export default defineConfig({
         host: '0.0.0.0',
         port: 5173,    
         hmr: {
-            host: '192.168.0.150',
+            host: '10.164.252.108',
         },
     },
 });
