07ed3052d994fea349a758dd19d60fa5d9fc6bd0
Danielinh0 <168349271+Danielinh0@users.noreply.github.com>
2026-05-12 08:30:05 -0600

    si

--- resources/views/components/file/⚡upload/upload.php
+++ resources/views/components/file/⚡upload/upload.php
@@ -4,20 +4,19 @@ use Livewire\Component;
 use Livewire\WithFileUploads;
 use Livewire\Attributes\Validate;
 use App\Services\UppaApiService;
+
 new class extends Component
 {
     use WithFileUploads;
 
-    #[Validate('file', 'required|file|max:10240')]
+    #[Validate('required|file|mimes:jpg,jpeg,png,webp|max:10240')]
     public $foto;
-    public $resultado = null;
-    public $cargando = false;
 
-    public $datos =null;
+    public $resultado = null;
+
     public function analizar(UppaApiService $uppaApiService)
     {
         $this->validate();
-        $this->cargando = true;
 
         $path = $this->foto->getRealPath();
 
